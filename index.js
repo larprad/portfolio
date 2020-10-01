54d33fe2f0e0e823aab45f0b2cddafbd4e32d9ef
larprad <59915248+larprad@users.noreply.github.com>
2020-10-01 18:31:04 +0200

    Work on popup

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
+// llp_test.surge.sh
+
 console.log('Hello there ✋');
+
+const popup = {
+  title: document.getElementById('popupTitle'),
+  description: document.getElementById('popupDescription'),
+  techno: document.getElementById('popupTechnoSection'),
+  link: document.getElementById('popupLinkSection'),
+  popupDiv: document.getElementById('popup'),
+  hide() {
+    this.popupDiv.style.display = 'none';
+  },
+  show() {
+    this.popupDiv.style.display = 'flex';
+  },
+  setTitle(x) {
+    this.title.innerHTML = x;
+  },
+  setDescription(x) {
+    this.description.innerHTML = x;
+  },
+  setTechno(x) {
+    const technoArray = x.map(
+      (index) => `<div class="technoBullet">
+      <img src=${technoSource[index][1]} alt=${technoSource[index][0]} class="miniIcon" />
+      <h5>${technoSource[index][0]}</h5>
+    </div>`
+    );
+    this.techno.innerHTML = technoArray.join('');
+  },
+  setLink(x) {
+    const linkArray = x.map(
+      (index) => `<div class="popupLink">
+      <p>${index[0]} :&nbsp</p>
+      <a href=${index[1]} target="blank">${index[1]}</a>
+    </div>`
+    );
+    this.link.innerHTML = '<h4 class="popupSubTitle">Liens</h4>' + linkArray.join('');
+  },
+  clean() {
+    this.title.innerHTML = '';
+    this.description.innerHTML = '';
+    this.link.innerHTML = '';
+    this.techno.innerHTML = '';
+  },
+};
+
+function handleTileClick(e) {
+  const tile = e.target.id;
+  if (tile) {
+    popup.setTitle(apps[tile].title);
+    popup.setDescription(apps[tile].description);
+    popup.setTechno(apps[tile].techno);
+    popup.setLink(apps[tile].link);
+    popup.show();
+  }
+}
+
+function handleClosePopup(e) {
+  if (e.target.id != 'innerContainer') {
+    popup.hide();
+  }
+}
+
+let projectTiles = Array.from(document.getElementsByClassName('projectTile'));
+projectTiles.forEach((x) => x.addEventListener('click', handleTileClick));
+document.getElementById('closePopup').addEventListener('click', handleClosePopup);
+document.getElementById('popup').addEventListener('click', handleClosePopup);
---
+++ scripts/source.js
@@ -0,0 +1,37 @@
+const apps = {
+  tetris: {
+    title: 'Tetris',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    link: [
+      ['Github', 'https://github.com/larprad/tetris'],
+      ['Lien direct', 'http://tetromino.surge.sh/'],
+    ],
+    techno: ['js', 'css', 'html'],
+  },
+  ohanami: {
+    title: 'Ohanami',
+    description: 'Oulala encore la belle description!',
+    link: [
+      ['Github', 'https://github.com/larprad/'],
+      ['Lien direct', 'http://ohanami.surge.sh/'],
+    ],
+    techno: ['js', 'css', 'html'],
+  },
+  oneWordPlease: {
+    title: 'One Word Please',
+    description: 'Oulala encore la belle descriptionnn blablablabalbla!',
+    link: [
+      ['Github', 'https://github.com/larprad/one_word_please'],
+      ['Lien direct', 'http://one-word-please.surge.sh/'],
+    ],
+    techno: ['react', 'js', 'css', 'html'],
+  },
+};
+
+const technoSource = {
+  js: ['JAVASCRIPT', './assets/icons/javascript.svg'],
+  css: ['CSS3', './assets/icons/css.svg'],
+  html: ['HTML5', './assets/icons/html.svg'],
+  react: ['REACT', './assets/icons/react-js.svg'],
+};
